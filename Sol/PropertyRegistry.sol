feb7adb735f6529dd77e75095a9e2983e0b1b7d6
Mac Lendl Lampa <82680646+lendllampa@users.noreply.github.com>
2021-10-23 02:42:53 -0400

    Update PropertyRegistry.sol

--- Sol/PropertyRegistry.sol
+++ Sol/PropertyRegistry.sol
@@ -1,14 +1,16 @@
-pragma solidity ^0.5.5;
+pragma solidity >=0.5.5;
 
 // UNFINISHED
 
+
 import "./TokenizeProperty.sol";
-// import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/access/Roles.sol";
 
 // @dev This contract allows investors to registerProperty for a crowdsale. 
 
-contract PropertyRegistry is PropertyCoinSaleDeployer {
+contract PropertyRegistry {
     using Roles for Roles.Role;
+    
+    address public sale_deployer_address;
     
     event PermissionGranted(address indexed account);
     event PermissionRevoked(address indexed account);
@@ -27,19 +29,43 @@ contract PropertyRegistry is PropertyCoinSaleDeployer {
         _;
     }
     
+    modifier onlyAuthorized() {
+        require(permissions.has(msg.sender), "Permission: Not approved");
+        _;
+    }
+    
+    function registerProperty(
+        string memory propertyAddress, 
+        string memory propertyTokenSymbol, 
+        uint tokenSupply, 
+        uint price, 
+        address payable beneficiary
+        ) public {
+            
+        require(permissions.has(msg.sender) || _owner == msg.sender, "Permission: Not approved");
+        
+        PropertyCoinSaleDeployer deployer = new PropertyCoinSaleDeployer(propertyAddress, propertyTokenSymbol, tokenSupply, price, beneficiary);
+        sale_deployer_address = address(deployer);
+    }
+    
     function grantPermission(address account) onlyOwner public {
         require(account != address(0), "Entered zero address.");
         permissions.add(account);
         emit PermissionGranted(account);
     }
     
+    function revokePermission(address account) onlyOwner public {
+        require(account != address(0), "Entered zero address.");
+        permissions.remove(account);
+        emit PermissionRevoked(account);
+    }
+    
     function hasPermission(address account) public view returns(bool) {
         return permissions.has(account);
     }
+    
 
 }
-
-
 
 
 
