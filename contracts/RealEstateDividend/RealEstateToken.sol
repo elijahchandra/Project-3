9b7c1396807ea0c54094576eabdb06877259d928
Mac Lendl Lampa <82680646+lendllampa@users.noreply.github.com>
2021-10-26 01:51:56 -0400

    Update RealEstateToken.sol

--- contracts/RealEstateDividend/RealEstateToken.sol
+++ contracts/RealEstateDividend/RealEstateToken.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.5.5;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20Detailed.sol";
+import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20Mintable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 contract RealEstateCoin is ERC20, ERC20Detailed, ERC20Mintable {
     using SafeMath for uint;
